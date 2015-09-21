1b6ce4442b6c0ee9250832e49951b67b3bc34b46
Michael Storgaard <git@mex.io>
2015-09-21 11:40:51 +0200

    Added environment config to initialize

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ var request = require('./lib/request');
 var app = express();
 app.use(bodyParser.json({limit: '50mb'}));
 
+var environment = process.argv[2] || 'testing';
+
 app.all('*', function (req, res) {
+    var url = 'https://' + (environment !== 'live' ? environment + '.' : '') + 'fitbay.com' + req.url;
     var body = req.body !== {} ? JSON.stringify(req.body) : undefined;
-    request(req.method, req.url, req.headers, body, function (code, headers, response) {
+    request(req.method, url, req.headers, body, function (code, headers, response) {
         res.set(headers).status(code).send(response);
     });
 });
