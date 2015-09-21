41a112617641cfcdf77e435af4f90e20fcdc7ea9
Michael Storgaard <git@mex.io>
2015-09-21 18:49:32 +0200

    Added environment check and fixed non-API calls

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ var request = require('./lib/request');
 var app = express();
 app.use(bodyParser.json({limit: '50mb'}));
 
-var environment = process.argv[2] || 'testing';
+var environment = process.argv[2];
+if (['staging', 'production'].indexOf(environment) === -1) {
+    environment = 'testing';
+}
 
 app.all('*', function (req, res) {
-    var url = 'https://' + (environment !== 'live' ? environment + '.' : '') + 'fitbay.com' + req.url;
+    if (!req.url.match(/^\/api/)) {
+        //Ignore non-API request
+        return;
+    }
+    var url = 'https://' + (environment !== 'production' ? environment + '.' : '') + 'fitbay.com' + req.url;
     var body = req.body !== {} ? JSON.stringify(req.body) : undefined;
     request(req.method, url, req.headers, body, function (code, headers, response) {
         res.set(headers).status(code).send(response);
